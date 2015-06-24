4b0f0b5dbddade7f2a132aade13b3bf406035ab8
Felix Wolff <felix.stefan.wolff@gmail.com>
2015-06-24 11:58:41 +0200

    About halfway there

--- pages.js
+++ pages.js
@@ -3,6 +3,7 @@
 var database = require('./database.js')
 var logger = require('winston')
 var q = require('q')
+var _ = require('lodash')
 
 exports.renderIndex = function(req, res) {
   if (req.isAuthenticated()) {
@@ -18,7 +19,37 @@ exports.renderIndex = function(req, res) {
 
 exports.renderJsStatistics = function(req,res) {
   database.getJsStatistics(req.user.id)
-    .then(function(data) { debugger; res.render('javascript', { user:req.user, statistics: data }) })
+    .then(function(data) {
+      res.render('javascript', { user:req.user, statistics: data })
+    })
+}
+
+exports.renderRecruiting = function(req,res) {
+  var promises = [
+    database.getLanguages()
+  ]
+
+  if (req.query.criteria) {
+    var langs = _.map(JSON.parse(req.query.criteria), 'language')
+    promises.push(database.getRankingFor(langs))
+  }
+
+  q.all(promises).then(function(results) {
+    if (results[1]) {
+      results[1].forEach(function(cf) {
+        cf.users.forEach(function(c) {
+          c.skills.forEach(function(s) {
+            s.timespan = s.timespan / 360
+          })
+        })
+      })
+    }
+
+    res.render("recruiting", {
+      languages: results[0],
+      candidateFields: results[1]
+    })
+  })
 }
 
 function renderLandingpage(res) {
@@ -45,8 +76,4 @@ function renderUserpage(req,res) {
       statistics: promises[1]
     })
   })
-}
-
-function renderRecruiterPage(req,res) {
-
 }
